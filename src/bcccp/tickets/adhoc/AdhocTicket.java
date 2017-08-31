ef438fe90575b74d560b3b64041adfa0a090f698
ITCSWV1 <31062733+ITCSWV1@users.noreply.github.com>
2017-09-01 09:58:52 +1000

    updated pay method

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -67,7 +67,8 @@ public class AdhocTicket implements IAdhocTicket {
 	@Override
 	public void pay(long dateTime, float charge) {
 		// TODO Auto-generated method stub
-		
+		paidDateTime = dateTime;
+		charge = charge;
 	}
 
 
